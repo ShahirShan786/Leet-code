a1f5ad22dffe95d02bf5c4a9d998e0ec5acdf817
Shahir_shan <166294249+ShahirShan786@users.noreply.github.com>
2026-06-25 12:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 150.5 MB (25.29%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int strStr(String haystack, String needle) {
+    int h = haystack.length;
+
+    for( int i = 0 ; i < h ; i++){
+        return haystack.indexOf(needle);
+    }
+    return -1;
+    
+  }
+}
